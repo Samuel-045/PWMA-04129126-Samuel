ad0afdc6e44e03251f870e5947d3bcb5fd83a89f
Samuel-045 <samuelvic856@gmail.com>
2023-09-26 23:11:09 -0300

    Upload semana 7

---
+++ PWMA - Semana#07/funcoes/cadastrohome.js
@@ -0,0 +1,50 @@
+const banco = [
+    {nomeCliente : 'neymar',
+    idade : 26,
+    email : 'alhilal@gmail.com',
+    senha : '1234'},
+
+    {nomeCliente : 'messi',
+    idade : 31,
+    emailCliente : 'goat@gmail.com',
+    senha : '1234'},
+
+    {nomeCliente : 'Marcos Leonardo',
+    idade : 19,
+    emailCliente : 'monstro@gmail.com',
+    senha : '1234'},
+
+]
+
+function cadastrohome() {
+    var nome = document.getElementById('campnome').value;
+    var email = document.getElementById('campemail').value;
+
+    const regex = /^[a-z0-9]+@[a-z0-9.-]+\.[a-z]{2,}$/i
+    var cond = regex.test(email)
+
+    var nomeExist = false 
+    var emailExis = false
+
+    banco.forEach((valorAtual, indice, arrayBanco) => {
+        if(valorAtual.nomeCliente == nome){
+            nomeExist = true
+        }else if(valorAtual.emailCliente == email){
+            emailExis = true
+        }
+
+    });
+
+    if (nome == false || email == false || nome.length<2 || cond == false) {
+        alert('Voce se cadastrou incorretamente, tente novamente!');
+    }else if(nomeExist==true){
+        alert("Nome já Cadastrado")
+    }else if(emailExis==true){
+        alert("Email já Cadastrado") 
+    }else{
+        alert('Voce se cadastrou com sucesso!!');
+    }
+
+
+}
+
